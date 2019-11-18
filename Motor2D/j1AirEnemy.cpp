9c2a2f92806dff9ac10a3220f3d0151040b7d465
oscarroyo4 <47557315+oscarroyo4@users.noreply.github.com>
2019-11-18 21:01:29 +0100

    enemy added

---
+++ Motor2D/j1AirEnemy.cpp
@@ -0,0 +1,62 @@
+#include "p2Defs.h"
+#include "p2Log.h"
+#include "j1App.h"
+#include "j1Render.h"
+#include "j1Scene.h"
+#include "j1FadeToBlack.h"
+#include "j1Map.h"
+#include "j1Input.h"
+#include "Animation.h"
+#include "j1AirEnemy.h"
+
+j1AirEnemy::j1AirEnemy() : j1Module()
+{
+	name.create("airenemy");
+
+	// idle animation (arcade sprite sheet)
+	idle.PushBack({ 0, 0, 32, 32 });
+	idle.speed = 1.0f;
+
+	fly.PushBack({ 32, 0, 32, 32 });
+	fly.PushBack({ 96, 0, 32, 32 });
+	fly.PushBack({ 64, 0, 32, 32 });
+	fly.PushBack({ 96, 0, 32, 32 });
+	fly.speed = 0.4f;
+
+	death.PushBack({ 0, 96, 32, 32 });
+	death.PushBack({ 32, 102, 32, 26 });
+	death.PushBack({ 64, 102, 32, 26 });
+	death.PushBack({ 96, 102, 32, 26 });
+	death.speed = 0.1f;
+	death.loop = false;
+
+	attack.PushBack({ 0, 32, 32, 32 });
+	attack.PushBack({ 32, 32, 32, 32 });
+	attack.PushBack({ 64, 32, 32, 32 });
+	attack.PushBack({ 96, 32, 32, 32 });
+	attack.speed = 0.2f;
+}
+
+// Destructor
+j1AirEnemy::~j1AirEnemy()
+{}
+
+bool j1AirEnemy::Awake(pugi::xml_node& config) {
+	bool ret = true;
+	LOG("Loading player from config_file");
+
+	texPath = config.child("path").attribute("tex").as_string();
+	hitPath = config.child("path").attribute("hit").as_string();
+	flyPath = config.child("path").attribute("jump").as_string();
+	attackPath = config.child("path").attribute("land").as_string();
+	life = config.child("propierties").attribute("life").as_int();
+	speed = config.child("propierties").attribute("speed").as_int();
+	gravity = config.child("propierties").attribute("gravity").as_float();
+	deathLimit = config.child("death").attribute("height").as_int();
+	initialX = config.child("initialPos1").attribute("x").as_int();
+	initialY = config.child("initialPos1").attribute("y").as_int();
+	initialX2 = config.child("initialPos2").attribute("x").as_int();
+	initialY2 = config.child("initialPos2").attribute("y").as_int();
+	punchTime = config.child("propierties").attribute("punchTime").as_int();
+	return ret;
+}
---
+++ Motor2D/j1AirEnemy.h
@@ -0,0 +1,96 @@
+#ifndef __j1AIRENEMY_H__
+#define __j1AIRENEMY_H__
+
+#include "j1Module.h"
+#include "j1Input.h"
+#include "j1Textures.h"
+#include "j1Audio.h"
+#include "j1Collision.h"
+#include "Animation.h"
+#include "p2Point.h"
+
+enum enemy_status {
+	AIRENEMY_IDLE,
+	AIRENEMY_FLY,
+	AIRENEMY_ATTACK,
+	AIRENEMY_ATTACK_FINISH,
+	AIRENEMY_DEATH
+};
+
+
+class j1AirEnemy : public j1Module
+{
+public:
+
+	j1AirEnemy();
+
+	// Destructor
+	virtual ~j1AirEnemy();
+
+	// Called before render is available
+	bool Awake(pugi::xml_node& config);
+
+	// Called each loop iteration
+	bool Start();
+
+	// Called each loop iteration
+	bool Update(float dt);
+
+	// Called each loop iteration
+	void Draw();
+
+	// Called before quitting
+	bool CleanUp();
+	bool Disable();
+
+	// Load / Save
+	bool Save(pugi::xml_node&) const;
+
+	// Collisions
+	bool OnGround();
+	bool WallCollision();
+
+	//Public variables
+	iPoint position;
+
+	int initialX;
+	int initialY;
+	int initialX2;
+	int initialY2;
+
+private:
+
+	bool ResetStates();
+
+	int life;
+	int speed;
+	float gravity;
+	int deathLimit;
+	fPoint vel;
+
+	bool atackEnable = true;
+	bool dead = false;
+
+	Uint32 punch_timer = 0;
+
+	p2SString texPath;
+	p2SString hitPath;
+	p2SString flyPath;
+	p2SString attackPath;
+	SDL_Texture* graphics;
+	int hitFx;
+	int flyFx;
+	int atackFx;
+	int punchTime;
+
+	Animation* current_animation = &idle;
+	Animation idle;
+	Animation fly;
+	Animation attack;
+	Animation death;
+
+	enemy_status status = AIRENEMY_IDLE;
+	SDL_Rect r;
+	Collider* colAirEnemy;
+};
+#endif //__j1AIRENEMY_H__
